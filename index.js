5a3cb94701aceb26bbf83214b7c401a00dec2107
Ariel Ross <arielross92@gmail.com>
2017-12-29 00:04:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,14 +35,11 @@ function appendKitten(name){
 function prependKitten(name){
   var kittens = ["Milo", "Otis", "Garfield"]
   return [name, ...kittens]
-  
 }
 
 function removeLastKitten(){
   var kittens = ["Milo", "Otis", "Garfield"]
   return kittens.slice(0, kittens.length - 1)
-
-  
 }
 
 function removeFirstKitten(){
